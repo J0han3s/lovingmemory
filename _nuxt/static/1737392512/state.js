8438366454359202e045ab9264c9b5e5051297cf
NoGoCodeHunter <NoGoCodeHunter@users.noreply.github.com>
2025-01-20 17:20:32 +0000

    deploy: 5d763824f006e55be7b75508cabf921c8584a9a3

--- _nuxt/static/1737392512/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1737392512",layout:"default",error:a,serverRendered:true,routePath:b,config:{name:"Cathy Johnson",dates:"June 19th 1946 - December 13, 2024",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ _nuxt/static/1737393632/live-stream/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1737393632",layout:"default",error:a,serverRendered:true,routePath:"\u002Flive-stream",config:{name:"Cathy Johnson",dates:"June 19th 1946 - December 13, 2024",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
