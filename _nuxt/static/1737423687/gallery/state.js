1ec8d91320072556637418ea8d8425ef857a2a72
NoGoCodeHunter <NoGoCodeHunter@users.noreply.github.com>
2025-01-21 01:45:21 +0000

    deploy: afae3c35c21011e37aac7b74dab607ab8fe94b21

--- _nuxt/static/1737423687/gallery/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1737423687",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgallery",config:{name:"Catharine Jane Fanning Johnson",dates:"June 19th 1946 - December 13, 2024",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1737423687/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1737423687",layout:"default",error:a,serverRendered:true,routePath:b,config:{name:"Catharine Jane Fanning Johnson",dates:"June 19th 1946 - December 13, 2024",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
