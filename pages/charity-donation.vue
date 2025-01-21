f51511c34c70b275246977a82056f748d10d02e3
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-01-20 16:45:46 -0800

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section details">
       <!-- CONTENT START -->
-      <h1 class="title">Charity Donations</h1>
+      <h1 class="title">Charitable Donation</h1>
       <p>Instead of flowers we would like to suggest people make a donation to the Michael J. Fox Foundation in the honor of Cathy. The work that the foundation has done ovMichaeone of the following charities</p>
       <a class="button" target="_blank" rel=”noopener” href="https://give.michaeljfox.org/donate ">Donate to Michael J Fox Foundation</a>
       <!-- CONTENT END -->
