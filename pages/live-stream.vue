0cdd48e9532a1e8027acde87939b63a076fae45d
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-01-23 15:41:46 -0800

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -6,7 +6,7 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Service Livestream</h1>
-      <p>For all those who can't attend the services in person, you can watch a livestream of the services from the Church's livestream below</p>
+      <p>For all those who can't attend the services in person, you can watch a livestream of the services from the Church's livestream below.</p>
       <p>
       <p>The livestream is expected to start 15 minutes before the service begins.
       </p>
