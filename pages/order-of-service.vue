548900b6788b977c5f8c162363ecdcf501442256
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-01-23 15:40:55 -0800

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -6,7 +6,7 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Order of Service</h1>
-      <p><em>Monday, January 27, 2025</em> will be held at Saint Joseph Marella Catholic Church, 7200 Auburn Folsom Road, Granite Bay Ca.</p>
+      <p><em>Monday, January 27, 2025</em> will be held at Saint Joseph Marella Catholic Church, 7200 Auburn Folsom Road, Granite Bay, Ca.</p>
       <p class="highlight">The service will be livestreamed via Youtube on the Churchs website. Please see Livestream link.</p>
       <h2>Church Service <span>11:00am</span></h2>
       <ul class="fa-ul">
