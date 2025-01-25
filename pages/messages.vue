74f94b9466030b79ec166c81559c56023ceca618
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-01-24 17:15:30 -0800

    Create messages.vue

---
+++ pages/messages.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <!-- CONTENT START -->
+      <h1 class="title">Messages</h1>
+      <p>If you wish to leave a message for family or friends please use the link below.</p>
+      <p class="highlight">Messages posted before January 27, 2025 will played at the memorial.</p
+      <p>All other messages left will be posted to this site once reviewed.</p
+      <!-- CONTENT END -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
