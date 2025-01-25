2ffc928d81e22f1b1790f1a459359fd96c2eca68
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-01-24 18:21:07 -0800

    Update messages.vue

--- pages/messages.vue
+++ pages/messages.vue
@@ -7,7 +7,7 @@
       <!-- CONTENT START -->
       <h1 class="title">Messages</h1>
       <p>If you wish to leave a message for family or friends please use the link below.</p>
-      <p class="highlight">Messages posted before January 27, 2025 will played at the memorial.</p
+      <p>Messages posted before January 27, 2025 will played at the memorial.</p
       <p>All other messages left will be posted to this site once reviewed.
       </p>
       <a class="button" target="_blank" rel=”noopener” href="https://docs.google.com/forms/d/e/1FAIpQLScsFSFA5chMcOanjkB2MmKRKp1QVG68f2tl2dTLjWCD8CAFtA/viewform">Leave a message for family and/or friends</a>
