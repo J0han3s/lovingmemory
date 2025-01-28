367032b33e2f407ab9b31a3b7d35216f8b947e65
NoGoCodeHunter <NoGoCodeHunter@users.noreply.github.com>
2025-01-28 03:35:35 +0000

    deploy: 5002c7ac2a095078d5d71039da8fa2303541b77e

---
+++ _nuxt/static/1738035334/charity-donation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1738035334",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcharity-donation",config:{name:"Catharine Jane Fanning Johnson",dates:"June 19, 1946 - December 13, 2024",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
