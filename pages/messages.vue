9ecddb66e7ec8eabf6d1e96a913512db57cc28f8
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-01-28 21:09:02 -0800

    Update messages.vue

--- pages/messages.vue
+++ pages/messages.vue
@@ -11,6 +11,7 @@
       <p>All other messages left will be posted to this site once reviewed.
       </p>
       <a class="button" target="_blank" rel=”noopener” href="https://docs.google.com/forms/d/e/1FAIpQLScsFSFA5chMcOanjkB2MmKRKp1QVG68f2tl2dTLjWCD8CAFtA/viewform">Leave a message for family and/or friends</a>
+      <img height="292" src="~/assets/message0001.jpg" alt="Message to Cathy" />
       <!-- CONTENT END -->
     </div>
   </div>
