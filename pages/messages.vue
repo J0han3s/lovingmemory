2d3f4aa5b726ec5b89c00f8171cd37b1e4f61901
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-03-09 07:41:58 -0700

    Update messages.vue

--- pages/messages.vue
+++ pages/messages.vue
@@ -7,7 +7,7 @@
       <!-- CONTENT START -->
       <h1 class="title">Messages</h1>
       <p>If you wish to leave a message for family or friends please use the link below.</p>
-      <p>Messages posted will be reviewed and posted in the coming weeks.</p>
+      <p>Messages posted will be reviewed and posted in the coming weeks. </p>
       <a class="button" target="_blank" rel=”noopener” href="https://docs.google.com/forms/d/e/1FAIpQLScsFSFA5chMcOanjkB2MmKRKp1QVG68f2tl2dTLjWCD8CAFtA/viewform">Leave a message for family and/or friends</a>
       <img height="292" src="~/assets/message0001.jpg" alt="Message to Cathy" />
       <!-- CONTENT END -->
