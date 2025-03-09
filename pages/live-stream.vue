2593ed6acc81c1d087fd07ba19581d937c4f9160
NoGoCodeHunter <40406869+NoGoCodeHunter@users.noreply.github.com>
2025-03-09 08:07:07 -0700

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -6,17 +6,15 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Service Livestream</h1>
-      <p>Below is a replay of the funeral serivce.
+      <p>Below is a replay of the funeral service.
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="https://youtu.be/9VwT-rJKbK4">Funeral Services Replay</a>
+      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/9VwT-rJKbK4?si=jNyi5da0TsSDbEnD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <!-- CONTENT END -->
     </div>
   </div>
 </template>
-
 <script>
 export default {}
 </script>
-
 <style lang="scss">
 </style>
